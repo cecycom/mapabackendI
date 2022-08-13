d302b9a3fa064d392e9a9f9ac2f5bf566b3c45e6
Kleber Petry <cecy.comcliente2@gmail.com>
2022-08-12 21:27:00 -0300

    Mapa

---
+++ README.md
@@ -0,0 +1,6 @@
+﻿## Olá, Sou Adriana Cecy Ferreira S Alves, 
+estudante do curso superior de Sistemas para Internet da Unicesumar, e desenvolvedor da Atividade MAPA - PROGRAMAÇÃO BACK END I - 53/2022.
+ 
+ - RA: 19116729-5
+ - Curso: Sistemas para Internet
+ - Ano: 2022
---
+++ pages/quem_somos.php
@@ -0,0 +1,16 @@
+<div class="prod-cl">
+        <img src="images/cozinheira.jpeg">
+        <div class="legend">Dona Rita</div>
+</div>
+<div class="prod-cr">
+    <h1>Quem somos</h1>
+    <p>
+    Em atividades desde 2010, as Marmitas de Dona Rita são um sucesso. Uma comida bem feita e suculenta, são os segredos.
+    Dona Rita é uma mulher empreendedora que desde de cedo já sabia o que queria fazer: ser cozinheira.
+    Sua dedicação e perspicácia traduzem a mulher vitoriosa que ela é hoje.
+    </p>
+       
+    <p>
+        Experimente... é uma delícia!
+    </p>
+</div>
